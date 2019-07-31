7ae8864aca99d2932e76b4898a0c1feea601586a
cmblue <german.sayago@gmail.com>
2019-07-31 14:11:31 -0300

    add organizations

---
+++ src/views/Organizations.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="organizations">
+   <table>
+     <tbody>
+       <tr v-for="user in users" :key="user.index">
+         <td>{{ user.email }}</td>
+       </tr>
+     </tbody>
+   </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      users: null
+    }
+  },
+  mounted() {
+    this.getUsers()    
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get('https://randomuser.me/api/?results=10')
+        .then( response => {
+          this.users = response.data.results
+        })
+        .catch( e => console.log(e))
+    } 
+  },
+}
+</script>
